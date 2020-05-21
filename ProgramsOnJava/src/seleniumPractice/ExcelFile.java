401df43fb19e7f26ea3abb1656402e2a0885208a
pankaj101010 <pankajdevgade10@gmail.com>
2020-05-21 15:29:26 +0530

    21-05-2020

--- ProgramsOnJava/src/seleniumPractice/ExcelFile.java
+++ ProgramsOnJava/src/seleniumPractice/ExcelFile.java
@@ -4,9 +4,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 
 public class ExcelFile {
 public static void main(String[] args) throws IOException {
@@ -14,6 +16,7 @@ public static void main(String[] args) throws IOException {
     FileInputStream fs = new FileInputStream("C:\\Users\\PANKAJ\\Desktop\\Test.xlsx");
     //throws IOException
     XSSFWorkbook book = new XSSFWorkbook(fs);
+    XSSFCell clt;
     XSSFSheet sheet = book.getSheet("Sheet1");
     //System.out.println(sheet.getRow(1).getCell(1).getStringCellValue());
     int row = sheet.getLastRowNum()+1;
@@ -24,7 +27,10 @@ public static void main(String[] args) throws IOException {
     for (int i=0;i<row;i++){
         for(int j=0;j<col;j++){
         	XSSFRow rw = sheet.getRow(i);
-            ls.add(sheet.getRow(i+1).getCell(j).getStringCellValue());
+             Cell cl = rw.getCell(j);
+            // if(cl.getCellType()==clt.) {
+          System.out.print(sheet.getRow(i+1).getCell(j).getStringCellValue()+" :");
+             //}
         }
         System.out.println();
     }
